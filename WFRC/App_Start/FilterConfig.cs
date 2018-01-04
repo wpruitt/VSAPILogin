55e5f8e3a8f77e6888cedf9550eb22f94e3858f0
Willie Pruitt <w_pruitt@hotmail.com>
2018-01-04 17:02:50 -0600

    index.html login feature firing properly and retrieving token as expected -- minor issues on pulled code to work out -- specifically storing token locally

--- WFRC/App_Start/FilterConfig.cs
+++ WFRC/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace WFRC
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new System.Web.Mvc.AuthorizeAttribute());
         }
     }
 }
--- WFRC/Scripts/JAC.js
+++ WFRC/Scripts/JAC.js
@@ -1,37 +1,45 @@
-﻿var data = {
-    Email: self.registerEmail(),
-    Password: self.registerPassword(),
-    ConfirmPassword: self.registerPassword2(),
-    FirstName: self.FirstName,
-    LastName: self.LastName,
-    Address1: self.Address1,
-    Address2: self.Address2,
-    City: self.City,
-    State: self.State,
-    ZipCode: self.Zipcode
-};
+﻿//var data = {
+//    Email: self.registerEmail(),
+//    Password: self.registerPassword(),
+//    ConfirmPassword: self.registerPassword2(),
+//    FirstName: self.FirstName,
+//    LastName: self.LastName,
+//    Address1: self.Address1,
+//    Address2: self.Address2,
+//    City: self.City,
+//    State: self.State,
+//    ZipCode: self.Zipcode
+//};
 
-$.ajax({
-    type: 'POST',
-    url: '/api/Account/Register',
-    contentType: 'application/json; charset=utf-8',
-    data: JSON.stringify(data)
-}).done(function (data) {
-    self.result("Done!");
-}).fail(showError);
+//$.ajax({
+//    type: 'POST',
+//    url: '/api/Account/Register',
+//    contentType: 'application/json; charset=utf-8',
+//    data: JSON.stringify(data)
+//}).done(function (data) {
+//    self.result("Done!");
+//}).fail(showError);
 
-var loginData = {
-    grant_type: 'password',
-    username: self.loginEmail(),
-    password: self.loginPassword()
-};
 
-$.ajax({
-    type: 'POST',
-    url: '/Token',
-    data: loginData
-}).done(function (data) {
-    self.user(data.userName);
-    //Cache the access token in session storage.
-    sessionStorage.setItem(tokenKey, data.access_token);
-}).fail(showError);
+$("#loginBtn").click(function () {
+    var loginData = {
+        grant_type: 'password',
+        username: $("#loginEmail").val(),
+        password: $("#loginPassword").val()
+    };
+    console.log("Clicked");
+    console.log("loginData",loginData);
+    $.ajax({
+        type: 'POST',
+        url: '/Token',
+        data: loginData
+    }).done(function (data) {
+        console.log("self", self);
+        console.log("sessionStorage", sessionStorage);
+        console.log("tokenKey", tokenKey);
+        console.log("data", data);
+        self.user(data.userName);
+        //Cache the access token in session storage.
+        sessionStorage.setItem(tokenKey, data.access_token);
+    }).fail(showError);
+});
